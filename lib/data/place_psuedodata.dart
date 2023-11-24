6c89c8cbb8ef7e5fbda57403fc2957a51ebd95b6
DelgaMatt <115049801+DelgaMatt@users.noreply.github.com>
2023-11-24 13:49:01 -0800

    place details styling setup. installed google maps flutter

--- lib/data/place_psuedodata.dart
+++ lib/data/place_psuedodata.dart
@@ -1,10 +1,11 @@
 import 'dart:io';
 
+import 'package:flutter/material.dart';
 import 'package:places/models/place_model.dart';
 
 var pseudoPlaces = [
   Place(
     title: 'Portland, Oregon', 
-    image: File('https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg'), 
+    image: File( const NetworkImage('https://www.hertz.com/content/dam/hertz/global/blog-articles/places-to-go/6-beautiful-spots-to-see-in-portland-oregon/View-of-Portland-Oregon-fall.rendition.xlarge.jpg').toString()), 
     location: const PlaceLocation(latitude: 45.5152, longitude: 122.6784, address: 'Portland, Oregon'))
 ];
