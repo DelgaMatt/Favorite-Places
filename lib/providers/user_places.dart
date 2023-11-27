27d27df705336b8de38ce1ded495d23a1d81260f
DelgaMatt <115049801+DelgaMatt@users.noreply.github.com>
2023-11-27 14:14:41 -0800

    added sql db interactivity

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -12,6 +12,40 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 class UserPlacesNotifier extends StateNotifier<List<Place>> {
   UserPlacesNotifier() : super(const []);
 
+  Future<Database> _getDatabase() async {
+    final dbPath = await sql.getDatabasesPath();
+    final db = await sql.openDatabase(
+      path.join(dbPath, 'places.db'),
+      onCreate: (db, version) async {
+        return db.execute(
+            'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT, lat REAL, lng REAL), address TEXT');
+      },
+      version: 1,
+    );
+    return db;
+  }
+
+  Future<void> loadPlaces() async {
+    final db = await _getDatabase();
+    final data = await db.query('user_places');
+
+    final places = data
+        .map(
+          (row) => Place(
+            id: row['id'] as String,
+            title: row['title'] as String,
+            image: File(row['image'] as String),
+            location: PlaceLocation(
+                latitude: row['lat'] as double,
+                longitude: row['lng'] as double,
+                address: row['address'] as String),
+          ),
+        )
+        .toList();
+
+    state = places;
+  }
+
   // this is how we're updating the state with riverpod
   void addPlace(String title, File image, PlaceLocation location) async {
     final appDir =
@@ -24,15 +58,15 @@ class UserPlacesNotifier extends StateNotifier<List<Place>> {
     final newPlace =
         Place(title: title, image: copiedImage, location: location);
 
-    final dbPath = await sql.getDatabasesPath();
-    sql.openDatabase(path.join(dbPath, 'places.db'),
-        onCreate: (db, version) async {
-          return db.execute(
-            'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT, lat REAL, lng REAL), addrress TEXT'
-            );
-        },
-        version: 1,
-    ); //if db not existing, opening it will create it
+    final db = await _getDatabase();
+    db.insert('user_places', {
+      'id': newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.image.path,
+      'lat': newPlace.location.latitude,
+      'lng': newPlace.location.longitude,
+      'address': newPlace.location.address
+    });
 
     //provided by StateNotifier
     state = [newPlace, ...state];
--- lib/screens/places_screen.dart
+++ lib/screens/places_screen.dart
@@ -5,17 +5,32 @@ import 'package:places/providers/user_places.dart';
 import 'package:places/screens/add_places_screen.dart';
 import 'package:places/widgets/places_list.dart';
 
-class PlacesScreen extends ConsumerWidget {
+class PlacesScreen extends ConsumerStatefulWidget {
   const PlacesScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<PlacesScreen> createState() {
+    return _PlacesScreenState();
+  }
+}
+
+class _PlacesScreenState extends ConsumerState<PlacesScreen> {
+
+  late Future<void> _placesFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _placesFuture = ref.read(userFavoritePlacesProvider.notifier).loadPlaces();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     // final userPlaces = ref.watch(userFavoritePlacesProvider);
 
     // seeded Data --- Uncomment above ------------------------->
     final userPlaces = ref.watch(placesProvider);
     // ---------------------------------------->
-
 
     return Scaffold(
       appBar: AppBar(
@@ -32,9 +47,13 @@ class PlacesScreen extends ConsumerWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8),
-        child: PlacesList(
-          places: userPlaces,
-        ),
+        child: FutureBuilder(
+          future: _placesFuture, 
+          builder: (context, snapshop) => 
+          snapshop.connectionState == ConnectionState.waiting 
+            ? const Center(child: CircularProgressIndicator(),)
+            : PlacesList(places: userPlaces,),
+          )
       ),
     );
   }
